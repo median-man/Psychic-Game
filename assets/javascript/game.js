58946b5a5d4f622fb948bbef0bb2be7342d674e4
John Desrosiers <johndrunner@yahoo.com>
2017-11-22 09:09:34 -0800

    refactor global vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,63 @@
-var solution, wins, losses, guessesLeft;
-
-// set starting values and start new round on page load
-wins = 0;
-losses = 0;
-newRound();
+// declare globals
+var guessesLeft = 0;
+var lettersUsed = [];
+var losses = 0;
+var max_guesses = 9;
+var solution = "";
+var wins = 0;
 
 // returns a random letter from the alphabet
 function getRandomLetter() {
-	// random number for charCodes for a to z inclusive
 	var code = Math.floor(Math.random() * (26)) + 97; 
 	return String.fromCharCode(code);
 }
 
-// main function for game that handles a guess from player
+// handle guess from player
 function handleGuess(guessedLetter) {
-	// if event.key is in lettersUsed then
+  // compare the guess letter with the solution and
+  // the previously guessed letters
 	if ( lettersUsed.indexOf(guessedLetter) > -1 ) {
-		alert("You already used " + guessedLetter + ".");
-
-	// else if event.key lowercase === solution 
+    alert("You already used " + guessedLetter + ".");
 	} else if ( guessedLetter === solution ) {
-		console.log("game won");
-		// then game is won
 		wins++;
-		newRound();			
-	// else decrement guessesLeft
+		newRound();
 	} else {
-		guessesLeft--;
+    guessesLeft--;
 		lettersUsed.push(guessedLetter);
-	}
+  }
+  
+  // game is lost if there are no more guesses
 	if ( guessesLeft === 0 ) {
-		// the game is lost, increment losses and start new round
 		losses++;
 		newRound();
 	}
-
+  
 	// update display
 	updateView(wins, losses, guessesLeft, lettersUsed);
 }
 
 // resets values for a new round
 function newRound() {
-	guessesLeft = 9;
+  guessesLeft = max_guesses;
 	solution = getRandomLetter();
 	lettersUsed = [];
 }
 
 // updates data displayed on page
-function updateView(w, l, remGuesses, arrLetters) {
-	// join array with commas before rendering
+function updateView(wins, losses, remGuesses, arrLetters) {
 	document.querySelector("#letters-used").textContent = arrLetters.join(", ");
-	document.querySelector("#wins").textContent = w;
-	document.querySelector("#losses").textContent = l;
+	document.querySelector("#wins").textContent = wins;
+	document.querySelector("#losses").textContent = losses;
 	document.querySelector("#guesses-left").textContent = remGuesses;
 }
 
 // runs game when key is pressed
 document.onkeyup = function(event) {
-	// key pressed is a to z inclusive
+  // key pressed is a to z inclusive
 	if ( event.keyCode >= 65 && event.keyCode <= 90 ) {
-		handleGuess(event.key.toLowerCase());
+    handleGuess(event.key.toLowerCase());
 	}
 };
+
+// start game
+newRound();
